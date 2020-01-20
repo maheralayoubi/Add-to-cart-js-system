a2607de5d57a391d3253ebc1e307af19e6306602
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-01-20 15:12:04 +0900

    Declaring cart variables

---
+++ app.js
@@ -0,0 +1,11 @@
+// Variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
