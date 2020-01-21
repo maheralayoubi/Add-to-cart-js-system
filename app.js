50ca080f2b0420b44a6c12dff419c3d618de10f1
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-01-21 12:27:08 +0900

    returning the products

--- app.js
+++ app.js
@@ -19,7 +19,27 @@ class Products {
         try {
             let result = await fetch("products.json");
             let data = await result.json();
-            return data;
+            let products = data.items;
+            // Products array
+            products = products.map(item => {
+                const {
+                    title,
+                    price
+                } = item.fields;
+                const {
+                    id
+                } = item.sys;
+                const {
+                    image
+                } = item.fields.image.fields.file.url;
+                return {
+                    title,
+                    price,
+                    id,
+                    image
+                }
+            })
+            return products;
         } catch (error) {
             console.log(error);
         }
