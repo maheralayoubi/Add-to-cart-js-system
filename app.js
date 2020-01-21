abe9552d399c933be96672a4e4bf8051aa1731e4
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-01-21 12:07:27 +0900

    Fetching data from json file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Variables
 
-const cartBtn = document.querySelector('.cart-btn');
-const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector('.clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartOverlay = document.querySelector('.cart-overlay');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-const cartContent = document.querySelector('.cart-content');
-const productsDOM = document.querySelector('.products-center');
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
 
 // Cart
 let cart = [];
@@ -17,8 +17,9 @@ let cart = [];
 class Products {
     async getProducts() {
         try {
-            let result = await fetch('products.json');
-            return result;
+            let result = await fetch("products.json");
+            let data = await result.json();
+            return data;
         } catch (error) {
             console.log(error);
         }
@@ -26,19 +27,15 @@ class Products {
 }
 
 // Display products
-class UI {
-
-}
+class UI {}
 
 // Local storage
-class Storage {
-
-}
+class Storage {}
 
 document.addEventListener("DOMContentLoaded", () => {
     // Instances
-    const ui = new UI()
-    const products = new Products()
+    const ui = new UI();
+    const products = new Products();
 
     // Get all products
     products.getProducts().then(data => console.log(data));
