7b133e20496b2849a9fb6eab62d3d17c0682e530
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-02-09 22:45:27 +0900

    Add the dynamic amount of items

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ class UI {
                     </div>
                     <div>
                         <i class="fas fa-chevron-up" data-id=${item.id}></i>
-                        <p class="item-amount">1</p>
+                        <p class="item-amount">${item.amount}</p>
                         <i class="fas fa-chevron-down data-id=${item.id}"></i>
                     </div>`;
     }
